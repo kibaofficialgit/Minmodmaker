d38cf006bb82dd5107cf29384991d15458a4b3db
kibaofficialgit <kibalvl133@gmail.com>
2026-05-08 03:57:16 +0530

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,4 +1,4 @@
-import { generateAddon } from "../../lib/generator";
+const { generateAddon } = require("../../lib/generator");
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
